d5ecf674cae508c1e2686353f6e206c0e5918453
Samir Parikh <siparikh@gmail.com>
2025-02-13 16:17:38 -0500

    add RSS feed generation for blog posts

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+
+export async function GET(context) {
+  const posts = await getCollection("blog");
+  return rss({
+    stylesheet: '/rss/pretty-feed-v3.xsl',
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
